36e6915816f9ae0f1eae0f38b808cb6732baeaed
tjfalusi <toni@falusi.tv>
2020-07-04 16:49:58 +1000

    Add files via upload

---
+++ twinkle_basic.py
@@ -0,0 +1,11 @@
+from microbit import *
+import music
+
+twinkle = ['C', 'C', 'G', 'G', 'A', 'A', 'G',
+            'F', 'F', 'E', 'E', 'D', 'D', 'C',
+            'G', 'G', 'F', 'F', 'E', 'E', 'D',
+            'G', 'G', 'F', 'F', 'E', 'E', 'D',
+            'C', 'C', 'G', 'G', 'A', 'A', 'G',
+            'F', 'F', 'E', 'E', 'D', 'D', 'C']
+
+music.play(twinkle)
